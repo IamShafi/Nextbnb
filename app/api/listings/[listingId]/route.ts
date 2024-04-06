5b1ff847cfa262488c44d3aeb93157b0d274f1ec
iamShafi <67453945+IamShafi@users.noreply.github.com>
2024-04-06 21:54:51 +0600

    Feature: Add PUT function for updating rent listing by ID

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -32,3 +32,33 @@ export async function DELETE(
 
   return NextResponse.json(listing);
 }
+
+// Function to handle PUT request
+export async function PUT(
+  request: Request, 
+  { params, body }: { params: IParams; body: any }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== 'string') {
+    throw new Error('Invalid ID');
+  }
+
+  const updatedListing = await prisma.listing.updateMany({
+    where: {
+      id: listingId,
+      userId: currentUser.id
+    },
+    data: {
+      ...body
+    }
+  });
+
+  return NextResponse.json(updatedListing);
+}
